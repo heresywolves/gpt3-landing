44268633a4b16095c3be526278ae3ecf494d8226
Howl Kimura <howlkimuradev@gmail.com>
2024-03-30 21:21:05 +0300

    Setup root styles in index.css

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import './navbar.css'
+import { RiMenu3Line, RiCloseLine } from 'react-icons/ri'
 
 function Navbar() {
   return (
     <div>
-      
+      Navbar
     </div>
   )
 }
