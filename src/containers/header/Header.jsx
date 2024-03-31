3722ffdcc8853f36b45777ba396c824a81bcd395
Howl Kimura <howlkimuradev@gmail.com>
2024-03-31 13:54:57 +0300

    Add styles to header left side

--- src/containers/header/Header.jsx
+++ src/containers/header/Header.jsx
@@ -11,7 +11,7 @@ function Header() {
         <p>Yet bed any for travelling assistance indulgence unpleasing. Not thoughts all exercise blessing. Indulgence way everything joy alteration boisterous the attachment. Party we years to order allow asked of.</p>
 
         <div className="gpt3__header-content__input">
-          <input type="email" placeholder='Your Email Adress' />
+          <input type="email" placeholder='Your Email Address' />
           <button type='button'>Get Started</button>
         </div>
 
@@ -20,9 +20,9 @@ function Header() {
           <p>1,600 people requested access a visit in last 24 hours</p>
         </div>
 
-        <div className="gpt3__header-image">
+        {/* <div className="gpt3__header-image">
           <img src={ai} alt="ai" />
-        </div>
+        </div> */}
       </div>
     </div>
   )
