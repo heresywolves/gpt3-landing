6dd66840c3b0455110d3287fbab3c4dcbb8f19cf
Howl Kimura <howlkimuradev@gmail.com>
2024-04-03 23:06:12 +0300

    Updates

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/gpt3-landing/"
+  base: "https://heresywolves.github.io/gpt3-landing/"
 })
