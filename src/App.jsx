9c842c26e28d5db37eab6e7425c02825115a7781
Howl Kimura <howlkimuradev@gmail.com>
2024-04-20 14:50:56 +0300

    Disable preloader

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ function App() {
 
   return (
     <div className='App'>
-      <Preloader />
+      {/* <Preloader /> */}
       <Favicon url={logo} />
       <div className="gradient__bg">
         <Navbar />
