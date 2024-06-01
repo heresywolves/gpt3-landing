109c26aab9fcdaa95d0d177b7c80cf068d547308
Your Namesifer <olegajun@gmail.com>
2024-06-01 20:24:29 +0300

    Convert some images to webp

--- src/components/brand/imports.js
+++ src/components/brand/imports.js
@@ -1,6 +1,6 @@
 import google from '../../assets/google.png';
 import slack from '../../assets/slack.png';
-import atlassian from '../../assets/atlassian.png';
+import atlassian from '../../assets/atlassian.webp';
 import dropbox from '../../assets/dropbox.png';
 import shopify from '../../assets/shopify.png';
 
